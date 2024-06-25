61666b017a0758500d4121394b6f64dd97ea5535
mickeyadams <103151344+mickeyadams@users.noreply.github.com>
2024-06-24 19:12:58 -0500

    New practice files

---
+++ Function Practices/TKinter_Main_code.py
@@ -0,0 +1,14 @@
+from tkinter import * #Importa la libreria de tkinter.
+
+pantalla = Tk() #define el nombre del contenedor.
+
+
+
+
+
+
+
+
+
+pantalla.mainloop() #ejecuta el la funcion de loop que ejecuta los eventos y las funciones puestas en el codigo
+
